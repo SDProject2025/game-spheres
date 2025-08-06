902fd48f4666d1ba5f70f3c01656f04df5e70bc7
EbrahimHoosen <ebrahimhoosen09@gmail.com>
2025-08-06 18:52:18 +0200

    refactor(style): updated UI for signup and signin
    refactored CSS and layout to improve styling and layout of auth pages without changing any functionality

--- src/components/auth/signUp/signUpForm.tsx
+++ src/components/auth/signUp/signUpForm.tsx
@@ -18,16 +18,35 @@ export default function SignUpForm({handleSignUpClick}: Props) {
         handleSignUpClick(username, email, password);
     }
     
-    return (
-        <form className="flex-col justify-evenly border border-green-500 rounded-2xl p-5 bg-gray-800" onSubmit={formSubmitHandler}>
-            <h1>Sign Up</h1>
-            <TextInput label="Username:" icon={<MdPerson/>} onChange={(e) => setUsername(e.target.value)}/>
-            <TextInput label="Email:" icon={<MdEmail/>} onChange={(e) => setEmail(e.target.value)}/>
-            <PasswordInput label="Password:" onChange={(e) => setPassword(e.target.value)}/>
+      return (
+    <div className="neon-rings-wrapper-blue">
+      <div className="neon-ring-blue neon-ring-blue-1"></div>
+      <div className="neon-ring-blue neon-ring-blue-2"></div>
+      <div className="neon-ring-blue neon-ring-blue-3"></div>
+      <form
+        className="relative w-[300px] p-8 bg-[#222]/20 text-white rounded-lg shadow-xl" onSubmit={formSubmitHandler}>
+        <h1 className="text-2xl text-[#3b82f6] tracking-wide mb-6 text-center">Sign Up</h1>
+        <TextInput
+          label="Username:"
+          icon={<MdPerson />}
+          onChange={(e) => setUsername(e.target.value)}
+        />
+        <TextInput
+          label="Email:"
+          icon={<MdEmail />}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <PasswordInput label="Password:" onChange={(e) => setPassword(e.target.value)} />
 
-            <div className="flex flex-col justify-center items-center pt-5">
-                <button type="submit" className="p-3 border rounded-2xl hover:bg-gradient-to-br hover:from-green-500 hover:to-green-950 transition-colors duration-200">Sign Up</button>
-            </div>
-        </form>
-    );
+        <div className="flex flex-col justify-center items-center pt-5">
+          <button
+            type="submit"
+            className="w-full py-2 mt-4 bg-[#3b82f6] text-[#111] font-semibold rounded-md shadow-[0_0_15px_#3b82f6] hover:bg-[#2563eb] transition"
+          >
+            Sign Up
+          </button>
+        </div>
+      </form>
+    </div>
+  );
 }

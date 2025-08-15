8bdbe549599101d9755a062aa33377043b99ae5b
Zayaan Omar <zayaan.omar@icloud.com>
2025-08-15 17:40:44 +0200

    chore(sidebar): wraps layout with sidebar
    layout.tsx wrapped with the sidebar so that each page has the sidebar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,28 @@
 import { ReactNode } from "react";
-import "@/styles/globals.css"
+import "@/styles/globals.css";
+import Sidebar from "@/components/Sidebar";
 export const metadata = {
-    title: "GameSpheres",
-    description: "yes",
+  title: "GameSpheres",
+  description: "yes",
 };
 
-export default function RootLayout({children}: {children: ReactNode}) {
-    return(
-        <html>
-            <body>
-                {children}
-            </body>
-        </html>
-    );
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className="flex">
+        {/* Fix sidebar so its unaffected by scroll*/}
+        <aside className="fixed left-0 top-0 h-screen w-64 bg-black text-white">
+          <Sidebar />
+        </aside>
+        {/* main pane of content can scroll */}
+        <main className="ml-64 flex-1 h-screen overflow-y-auto">
+          {children}
+        </main>
+      </body>
+    </html>
+  );
 }

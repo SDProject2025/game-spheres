af081df916d08ae3b44b0045c381d69b90e914c8
Liam Binedell <liam.binedell@gmail.com>
2025-08-15 18:16:46 +0200

    feat(sidebar): create sidebar wrapper
    Created sidebarWrapper.tsx to facilitate client side conditional rendering

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,6 @@
-import { ReactNode } from "react";
 import { UserProvider } from "@/config/userProvider";
-
+import SidebarWrapper from "@/components/sidebar/sidebarWrapper";
 import "@/styles/globals.css";
-import Sidebar from "@/components/Sidebar";
 export const metadata = {
   title: "GameSpheres",
   description: "yes",
@@ -16,11 +14,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <UserProvider>
+        
         <body className="flex">
           {/* Fix sidebar so its unaffected by scroll*/}
-          <aside className="fixed left-0 top-0 h-screen w-64 bg-black text-white">
-            <Sidebar />
-          </aside>
+          <SidebarWrapper/>
           {/* main pane of content can scroll */}
           <main className="ml-64 flex-1 h-screen overflow-y-auto">
             {children}

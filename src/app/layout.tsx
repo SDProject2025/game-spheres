3dfb4538e00d778da2f2b8826411fb56944624d5
Liam Binedell <liam.binedell@gmail.com>
2025-08-15 18:04:44 +0200

    refactor(layout): formatting

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,7 +8,6 @@ export const metadata = {
   description: "yes",
 };
 
-        
 export default function RootLayout({
   children,
 }: {
@@ -17,17 +16,17 @@ export default function RootLayout({
   return (
     <html lang="en">
       <UserProvider>
-      <body className="flex">
-        {/* Fix sidebar so its unaffected by scroll*/}
-        <aside className="fixed left-0 top-0 h-screen w-64 bg-black text-white">
-          <Sidebar />
-        </aside>
-        {/* main pane of content can scroll */}
-        <main className="ml-64 flex-1 h-screen overflow-y-auto">
-          {children}
-        </main>
-      </body>
-        </UserProvider>
+        <body className="flex">
+          {/* Fix sidebar so its unaffected by scroll*/}
+          <aside className="fixed left-0 top-0 h-screen w-64 bg-black text-white">
+            <Sidebar />
+          </aside>
+          {/* main pane of content can scroll */}
+          <main className="ml-64 flex-1 h-screen overflow-y-auto">
+            {children}
+          </main>
+        </body>
+      </UserProvider>
     </html>
   );
 }

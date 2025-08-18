ff9a7e12418668fcd2c94c7a0b46affb8fca66e4
Zayaan Omar <zayaan.omar@icloud.com>
2025-08-18 16:33:54 +0200

    fix(sidebarWrapper): determine correct sidebar width
    Now determines the correct width of the sidebar using the sidebarProvider cofig

--- src/components/sidebar/sidebarWrapper.tsx
+++ src/components/sidebar/sidebarWrapper.tsx
@@ -1,14 +1,27 @@
 "use client";
 import { useUser } from "@/config/userProvider";
-
+import { useSidebar } from "@/config/sidebarProvider";
 import Sidebar from "./sidebar";
+import { useEffect } from "react";
 
 export default function SidebarWrapper() {
-    const { user, loading } = useUser();
+  const { user, loading } = useUser();
+  const { isExpanded } = useSidebar();
 
-    if (!user) return null;
+  useEffect(() => {
+    if (!user) {
+      document.documentElement.style.setProperty("--sidebar-width", "0px");
+    }
+  }, [user]);
+
+  if (!user) return null;
+
   return (
-    <aside className="fixed left-0 top-0 h-screen w-64 bg-black text-white">
+    <aside
+      className={`fixed left-0 top-0 h-screen bg-black text-white z-10 transition-all duration-300 ease-in-out ${
+        isExpanded ? "w-64" : "w-16"
+      }`}
+    >
       <Sidebar />
     </aside>
   );

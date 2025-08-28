d20c70e60006b534b897be11df007a386989539b
Liam-Binedell <liam.binedell@gmail.com>
2025-08-28 23:43:07 +0200

    feat(userSearch): create userSearch page

--- src/app/searchUsers/page.tsx
+++ src/app/searchUsers/page.tsx
@@ -1,4 +1,49 @@
+"use client";
+import SearchBar, { SearchItem } from "@/components/search/searchBar";
+import SearchItemContainer from "@/components/search/searchItem";
+import ProfilePage from "@/components/profile/profilePage";
+
+interface User extends SearchItem {
+  uid: string;
+  profileUrl: string;
+  username: string;
+  displayName: string;
+  bio: string;
+  followers: string[];
+  following: string[];
+  posts: number;
+}
+
+async function searchWithQuery(query: string) {
+  try {
+    const users: User[] = [];
+    const res = await fetch(`/api/profile/search?query=${query}`);
+    const data = await res.json();
+    for (let user of data.users) users.push(user);
+    console.log(users);
+    return users;
+  } catch (e: unknown) {
+    console.error("Error searching:", e);
+    return [];
+  }
+}
+
+const renderSearchItem = (user: User, isSelected: boolean) => (
+  <SearchItemContainer searchTitle={user.username} imageUrl={user.profileUrl} />
+);
+
+const renderSearchDetails = (user: User) => <ProfilePage profile={user} />;
+
 export default function searchUsers() {
-    return <>
+  return (
+    <>
+      <SearchBar
+        placeholder="Search for other users..."
+        title="Search Users"
+        renderItem={renderSearchItem}
+        renderDetails={renderSearchDetails}
+        searchFunction={searchWithQuery}
+      />
     </>
+  );
 }

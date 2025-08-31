68a20165c5802d847ad88930fb3ab0bf5cf0b4f4
Liam-Binedell <liam.binedell@gmail.com>
2025-08-31 21:00:33 +0200

    refactor(ProfileButton): isolated "View Profile" into separate component

--- src/components/profile/profileButton.tsx
+++ src/components/profile/profileButton.tsx
@@ -3,12 +3,9 @@ import Link from "next/link";
 
 interface ProfileButtonProps {
   isOwner: boolean;
-  
 }
 
-export default function ProfileButton({ isOwner }: ProfileButtonProps) {
-  if (isOwner) {
-    
+export default function ProfileButton() {
     return (
       <Link href="/profile/edit">
         <button className="mt-6 px-6 py-2 rounded-md font-semibold text-[#111] bg-[#00ffc3] shadow-[0_0_15px_#00ffc3] hover:bg-[#00e6b3] transition flex items-center gap-2">
@@ -16,13 +13,4 @@ export default function ProfileButton({ isOwner }: ProfileButtonProps) {
         </button>
       </Link>
     );
-  } else {
-    return (
-      <button type="button" className="mt-6 px-6 py-2 rounded-md font-semibold text-[#111] bg-[#00ffc3] shadow-[0_0_15px_#00ffc3] hover:bg-[#00e6b3] transition flex items-center gap-2">
-        <FiUser/> View Profile
-      </button>
-    )
-  }
-
-
 }

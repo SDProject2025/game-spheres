07efb90aaa9abe33d6d8efb0eb39f33dfb62d7ac
Liam Binedell <liam.binedell@gmail.com>
2025-09-01 12:14:48 +0200

    feat(editProfile): create route that updates firestore fields for user

---
+++ src/app/api/profile/update/route.ts
@@ -0,0 +1,26 @@
+import { NextRequest, NextResponse } from "next/server";
+import { db } from "@/config/firebaseAdminConfig";
+import type { Profile } from "@/types/Profile";
+import { USERS_COLLECTION } from "../../collections";
+
+export async function POST(request: NextRequest) {
+  const body = await request.json();
+  if (!body)
+    return NextResponse.json({ message: "Missing post body" }, { status: 400 });
+  const profile: Profile = body;
+  console.log(profile.uid);
+  try {
+    await db.collection(USERS_COLLECTION).doc(profile.uid).update({
+      displayName: profile.displayName,
+      username: profile.username,
+      bio: profile.bio,
+      photoURL: profile.photoURL,
+    });
+
+    return NextResponse.json({success: true});
+  } catch (e: unknown) {
+    const message = e instanceof Error ? e.message : "Error updating profile";
+    console.error(message);
+    return NextResponse.json({message}, {status: 500});
+  }
+}

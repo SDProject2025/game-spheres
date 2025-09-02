bb9384af476d7de0962517f61744ef857b85713b
Zayaan Omar <zayaan.omar@icloud.com>
2025-09-02 12:38:05 +0200

    style(Feedback Body): format code

--- src/components/userFeedback/messageBody.tsx
+++ src/components/userFeedback/messageBody.tsx
@@ -3,19 +3,18 @@ type Props = {
   onChange: (value: string) => void;
 };
 
-
 export default function MessageBody({ value, onChange }: Props) {
-    return (
-        <div className="relative mb-4">
-            <textarea
-                id="message"
-                name="message"
-                rows={6}
-                value={value}
-                onChange={(e) => onChange(e.target.value)}
-                placeholder="Enter your message"
-                className="w-full p-3 border rounded-lg focus:ring-2 focus:ring-blue-500 focus:outline-none"
-            />
-        </div>
-    );
+  return (
+    <div className="relative mb-4">
+      <textarea
+        id="message"
+        name="message"
+        rows={6}
+        value={value}
+        onChange={(e) => onChange(e.target.value)}
+        placeholder="Enter your message"
+        className="w-full p-3 border rounded-lg focus:ring-2 focus:ring-blue-500 focus:outline-none"
+      />
+    </div>
+  );
 }

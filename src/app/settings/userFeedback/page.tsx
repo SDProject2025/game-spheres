f3acece2bd79dc6eba785b2ff3e87abf785bc48a
Zayaan Omar <zayaan.omar@icloud.com>
2025-09-02 12:37:23 +0200

    fix(Feedback Form): Fix sidebar overlap

--- src/app/settings/userFeedback/page.tsx
+++ src/app/settings/userFeedback/page.tsx
@@ -1,4 +1,4 @@
-'use client';
+"use client";
 
 import { useState } from "react";
 import MessageBody from "@/components/userFeedback/messageBody";
@@ -6,39 +6,38 @@ import NeonButton from "../../../components/neonButton";
 import { Toaster, toast } from "react-hot-toast";
 
 export default function FeedbackForm() {
-    const [message, setMessage] = useState("");
+  const [message, setMessage] = useState("");
 
-    const handleSubmit = async (e: React.FormEvent) => {
-        e.preventDefault();
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
 
-        await toast.promise(
-            fetch("../../api/settings/feedback", {
-                method: "POST",
-                headers: { "Content-Type": "application/json" },
-                body: JSON.stringify({ message }),
-            }),
-            {
-                loading: "Sending feedback...",
-                success: "Feedback sent! Thank you 😊",
-                error: "Failed to send feedback. Please try again.",
-            }
-        );
+    await toast.promise(
+      fetch("../../api/settings/feedback", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ message }),
+      }),
+      {
+        loading: "Sending feedback...",
+        success: "Feedback sent! Thank you 😊",
+        error: "Failed to send feedback. Please try again.",
+      }
+    );
 
-        setMessage("");
-    }
+    setMessage("");
+  };
 
-    return (
-        <>
-            <Toaster position="top-center" />
-            <form
-                onSubmit={handleSubmit}
-                className="max-w md mx-auto p-4 border rounded-x1 shadow-md"
-            >
-                <h2 className="text-x1 font semibold mb-4">We'd love your feedback!</h2>
-                <MessageBody value={message} onChange={setMessage} />
-                <NeonButton type="submit">SUBMIT</NeonButton>
-            </form>
-        </>
-        
-    )
+  return (
+    <>
+      <Toaster position="top-center" />
+      <form
+        onSubmit={handleSubmit}
+        className="max-w-2xl mx-auto p-4 border rounded-x1 shadow-md"
+      >
+        <h2 className="text-x1 font semibold mb-4">We'd love your feedback!</h2>
+        <MessageBody value={message} onChange={setMessage} />
+        <NeonButton type="submit">SUBMIT</NeonButton>
+      </form>
+    </>
+  );
 }

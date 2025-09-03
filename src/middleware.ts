0f28e988ae058a2681e771e6b158879594697533
Liam-Binedell <liam.binedell@gmail.com>
2025-09-03 17:13:16 +0200

    feat(auth-middleware): run on specific http methods rather than paths

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,32 +1,34 @@
 import { NextRequest, NextResponse } from "next/server";
 import { auth } from "./config/firebaseAdminConfig";
 
+const MUTATING_METHODS = ["POST", "PUT", "PATCH", "DELETE"];
+
 export async function middleware(request: NextRequest) {
-    const url = request.nextUrl;
+  const url = request.nextUrl;
 
-    if (url.pathname.startsWith("/api/")) {
-        const authHeader = request.headers.get("Authorization");
-        const token = authHeader?.split("Bearer ")[1];
+  if (
+    url.pathname.startsWith("/api/") &&
+    MUTATING_METHODS.includes(request.method)
+  ) {
+    const authHeader = request.headers.get("Authorization");
+    const token = authHeader?.split("Bearer ")[1];
 
-        if (!token) {
-            return NextResponse.json({message: "Unauthorized"}, {status: 401});
-        }
-
-        try {
-            const decoded = await auth.verifyIdToken(token);
-            request.headers.set("x-user-uid", decoded.uid);
-        } catch (err) {
-            return NextResponse.json({message: "Invalid token"}, {status: 403});
-        }
+    if (!token) {
+      return NextResponse.json({ message: "Unauthorized" }, { status: 401 });
     }
 
-    return NextResponse.next();
+    try {
+      const decoded = await auth.verifyIdToken(token);
+      // Add the verified UID to request headers for downstream handlers
+      request.headers.set("x-user-uid", decoded.uid);
+    } catch (err) {
+      return NextResponse.json({ message: "Invalid token" }, { status: 403 });
+    }
+  }
+
+  return NextResponse.next();
 }
 
 export const config = {
-  matcher: [
-    "/api/**/create/:path*",
-    "/api/**/update/:path*",
-    "/api/**/delete/:path*",
-  ],
+  matcher: ["/api/:path*", "api/chat/:path"],
 };

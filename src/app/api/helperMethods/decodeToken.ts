0431e86bd942ebef3bd4c6019e67679e8f793408
Liam-Binedell <liam.binedell@gmail.com>
2025-09-10 15:03:29 +0200

    refactor(auth-token): comment decodeToken.ts

--- src/app/api/helperMethods/decodeToken.ts
+++ src/app/api/helperMethods/decodeToken.ts
@@ -1,7 +1,11 @@
 import { auth } from "@/config/firebaseAdminConfig";
 
 export async function decodeToken(authHeader: string) {
-  const token = authHeader.split('Bearer ')[1] as string;
+  // Necessary as authHeader comes in form:
+  // Authorization: "Bearer [token]"
+  const token = authHeader.split("Bearer ")[1] as string;
   const decoded = await auth.verifyIdToken(token);
+
+  // Possibly redundant, but allows for more explicit handling on caller
   return decoded ? decoded : undefined;
 }

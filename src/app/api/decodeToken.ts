8231d6d565574f4145cc4920ed10f9c25c0ee973
Liam-Binedell <liam.binedell@gmail.com>
2025-09-10 15:11:02 +0200

    fix(auth-token): catch invalid token error safely

--- src/app/api/decodeToken.ts
+++ src/app/api/decodeToken.ts
@@ -4,8 +4,12 @@ export async function decodeToken(authHeader: string) {
   // Necessary as authHeader comes in form:
   // Authorization: "Bearer [token]"
   const token = authHeader.split("Bearer ")[1] as string;
-  const decoded = await auth.verifyIdToken(token);
-
-  // Possibly redundant, but allows for more explicit handling on caller
-  return decoded ? decoded : undefined;
+  try {
+    const decoded = await auth.verifyIdToken(token);
+    return decoded;
+  } catch (e) {
+    const message = e instanceof Error ? e.message : "Invalid token";
+    console.error(message);
+    return undefined;
+  }
 }

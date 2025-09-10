a55e6058f5bf874103cac4c14c979b29b03c1c09
Liam-Binedell <liam.binedell@gmail.com>
2025-09-10 15:27:30 +0200

    fix(token-auth): add check for undefined header

--- src/app/api/decodeToken.ts
+++ src/app/api/decodeToken.ts
@@ -1,12 +1,13 @@
 import { auth } from "@/config/firebaseAdminConfig";
 
-export async function decodeToken(authHeader: string) {
+export async function decodeToken(authHeader: string | null): Promise<string | undefined> {
+  if (!authHeader) return undefined;
   // Necessary as authHeader comes in form:
   // Authorization: "Bearer [token]"
   const token = authHeader.split("Bearer ")[1] as string;
   try {
     const decoded = await auth.verifyIdToken(token);
-    return decoded;
+    return decoded.uid;
   } catch (e) {
     const message = e instanceof Error ? e.message : "Invalid token";
     console.error(message);

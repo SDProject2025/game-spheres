7e32ece64ebf287b60963202076b5e847f9bce99
Liam-Binedell <liam.binedell@gmail.com>
2025-09-23 14:01:29 +0200

    feat(createMessage): create notification on message send
    adds document to user subcollection, "notifications"

--- src/app/api/chat/create/message/route.ts
+++ src/app/api/chat/create/message/route.ts
@@ -4,7 +4,8 @@ import {
   USERS_COLLECTION,
   CONVERSATIONS_COLLECTION,
 } from "@/app/api/collections";
-import { MessageInput } from "@/types/Message";
+import type { MessageInput } from "@/types/Message";
+import type { Notification } from "@/types/Notification";
 import {
   Timestamp,
   DocumentReference,
@@ -13,9 +14,11 @@ import {
 import { decodeToken } from "@/app/api/decodeToken";
 
 export async function POST(request: NextRequest) {
+  // verify sender token for security
   const authHeader = request.headers.get("Authorization");
   const uid = await decodeToken(authHeader);
 
+  // standard server side validation and input collection
   if (!uid) {
     return NextResponse.json({ message: "Unauthorized" }, { status: 401 });
   }
@@ -27,20 +30,25 @@ export async function POST(request: NextRequest) {
   const message = body as MessageInput;
 
   try {
+    // get a document reference to the current conversation
     const conversationRef = db
       .collection(CONVERSATIONS_COLLECTION)
       .doc(message.conversationId);
 
     const batch: WriteBatch = db.batch();
-    const now = Timestamp.now();
+    const now = Timestamp.now(); // keep now as a constant for consistency across fields
 
+    //get document reference to a new message within the messages subcollection
     const messageRef = conversationRef.collection("messages").doc();
+
+    // add message creation to batch
     batch.set(messageRef, {
       content: message.content,
       senderId: message.senderId,
       createdAt: now,
     });
 
+    // add new message as lastMessage to batch
     batch.set(
       conversationRef,
       {
@@ -55,17 +63,42 @@ export async function POST(request: NextRequest) {
       { merge: true }
     );
 
-    await batch.commit();
-
+    // pull conversation data to reference participants
     const conversationSnap = await conversationRef.get();
     const conversationData = conversationSnap.exists
       ? conversationSnap.data()
       : null;
 
+    // filter out message sender from list of participants
+    const participants: string[] = conversationData?.participants || [];
+    const recipients = participants.filter((id) => id !== message.senderId);
+
+    // create list of recipient document references
+    const recipientRefs = recipients.map((id) =>
+      db.collection(USERS_COLLECTION).doc(id)
+    );
+
+    // create type enforced notification
+    const notification: Notification = {
+      type: "message",
+      fromUid: message.senderId,
+      conversationId: message.conversationId,
+      messageId: messageRef.id,
+      createdAt: now,
+      read: false
+    }
+
+    // for each recipient reference, create a document in the notifications subcollection
+    for (const ref of recipientRefs) {
+      const notifRef = ref.collection("notifications").doc();
+      batch.set(notifRef, notification);
+    }
+
+    // finalise batch write
+    await batch.commit();
+
     return NextResponse.json({
       success: true,
-      messageId: messageRef.id,
-      conversation: conversationData,
     });
   } catch (e: unknown) {
     const message = e instanceof Error ? e.message : "Failed to post message";

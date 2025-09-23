1f9220444372e1adf090ba0437552db199c3a81d
Liam-Binedell <liam.binedell@gmail.com>
2025-09-23 14:01:49 +0200

    fix(notification): update notification type to support firestore timestamps

--- src/types/Notification.ts
+++ src/types/Notification.ts
@@ -1,11 +1,14 @@
+import { Timestamp } from "firebase-admin/firestore";
+
 export type Notification = {
     type: "like" | "comment" | "follow" | "message"; // self explanatory
     fromUid: string; // id of person who triggered notification
     // following are related to notification type
     postId?: string; 
     commentId?: string;
+    conversationId?: string;
     messageId?: string;
     ////
-    createdAt: number; 
+    createdAt: number | Timestamp; 
     read: boolean;
 }

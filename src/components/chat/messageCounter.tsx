528459bea687ad0bb4001b13ad1125b7c7403fc5
Liam-Binedell <liam.binedell@gmail.com>
2025-09-25 21:39:59 +0200

    fix(messageCounter): correctly count number of unreads

--- src/components/chat/messageCounter.tsx
+++ src/components/chat/messageCounter.tsx
@@ -1,8 +1,8 @@
-import { Mail } from "lucide-react";
+import { MessageCircleMore } from "lucide-react";
 import { db } from "@/config/firebaseConfig";
 import { useState, useEffect } from "react";
 import { useUser } from "@/config/userProvider";
-import { collection, onSnapshot, query } from "firebase/firestore";
+import { collection, onSnapshot, query, where } from "firebase/firestore";
 import { CONVERSATIONS_COLLECTION } from "@/app/api/collections";
 
 export default function ChatIcon() {
@@ -12,14 +12,21 @@ export default function ChatIcon() {
   useEffect(() => {
     if (!user) return;
 
-    const q = query(collection(db, CONVERSATIONS_COLLECTION));
+    // Only fetch conversations where user is a participant
+    const q = query(
+      collection(db, CONVERSATIONS_COLLECTION),
+      where("participants", "array-contains", user.uid)
+    );
 
     const unsub = onSnapshot(q, (snapshot) => {
       let total = 0;
       snapshot.docs.forEach((doc) => {
         const data = doc.data();
+        console.log(data.unreadCounts?.[user.uid]);
         total += data.unreadCounts?.[user.uid] || 0;
       });
+
+      console.log("unread total:", total);
       setUnreadCount(total);
     });
 
@@ -28,9 +35,9 @@ export default function ChatIcon() {
 
   return (
     <div className="relative inline-block">
-      <Mail className="w-6 h-6" />
+      <MessageCircleMore className="w-6 h-6" />
       {unreadCount > 0 && (
-        <span className="absolute -top-1 -right-1 w-5 h-5 flex items-center justify-center rounded-full text-green-500 text-xs font-bold">
+        <span className="absolute -top-1 -right-1 w-4 h-4 flex items-center justify-center rounded-full bg-green-500 text-xs font-bold">
           {unreadCount}
         </span>
       )}

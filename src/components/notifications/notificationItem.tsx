469f62a4d95a9af4103fab6ad0596462a3d82ad1
Liam Binedell <liam.binedell@gmail.com>
2025-09-30 14:18:41 +0200

    fix(notifications): fix escape character

--- src/components/notifications/notificationItem.tsx
+++ src/components/notifications/notificationItem.tsx
@@ -24,18 +24,19 @@ export default function NotificationItem({ notif, profiles, handlePlayClip }: Pr
       </div>
     );
   }
-  
+
   const username = notifSender.username;
   const isClipNotification = notif.type === "comment" || notif.type === "like";
 
-  if (!notifSender || isClipNotification === undefined) return <h1>Problem</h1>
+  if (!notifSender || isClipNotification === undefined) return <h1>Problem</h1>;
 
   let message: ReactNode = "";
   if (notif.type === "comment") {
     message = (
       <>
         <strong>{username}</strong> commented on your post:
-        <br />&quot{notif.commentContent}&quot
+        <br />
+        {`"${notif.commentContent}"`}
       </>
     );
   } else if (notif.type === "follow") {
@@ -53,27 +54,30 @@ export default function NotificationItem({ notif, profiles, handlePlayClip }: Pr
   }
 
   return (
-      <div className="flex items-start" onClick={() => {
+    <div
+      className="flex items-start"
+      onClick={() => {
         if (isClipNotification && notif.clip) handlePlayClip(notif.clip);
-        else router.push(`/profile/${notif.fromUid}`)
-      }}>
-        <img
-          src={notifSender.photoURL}
-          className="w-12 h-12 object-cover rounded-full"
-        />
-        <div className="flex flex-col pl-2">
-          <span>{message}</span>
-          <span className="text-sm text-gray-400">
-            {notif.createdAt
-              ? new Intl.DateTimeFormat([], {
-                  month: "short",
-                  day: "2-digit",
-                  hour: "2-digit",
-                  minute: "2-digit",
-                }).format(new Date(notif.createdAt))
-              : ""}
-          </span>
-        </div>
+        else router.push(`/profile/${notif.fromUid}`);
+      }}
+    >
+      <img
+        src={notifSender.photoURL}
+        className="w-12 h-12 object-cover rounded-full"
+      />
+      <div className="flex flex-col pl-2">
+        <span>{message}</span>
+        <span className="text-sm text-gray-400">
+          {notif.createdAt
+            ? new Intl.DateTimeFormat([], {
+                month: "short",
+                day: "2-digit",
+                hour: "2-digit",
+                minute: "2-digit",
+              }).format(new Date(notif.createdAt))
+            : ""}
+        </span>
       </div>
+    </div>
   );
 }
